32142316263980f6d5a561bf69f32722a0fd30f1
Kathy Walrath <kathyw@google.com>
2013-01-08 15:17:34 -0800

    Fix issue 9: String adjacent literals typo.

--- code/ch02/adjacent_string_literals.dart
+++ code/ch02/adjacent_string_literals.dart
@@ -1,6 +1,6 @@
 main() {
   // BEGIN(adjacent_string_literals) 
-  var s = 'String ''concatenation'
+  var s = 'String ' 'concatenation'
           " works even over line breaks.";
   assert(s == 'String concatenation works even over line breaks.');
   // END(adjacent_string_literals)
