b99fb620b51635d34f28d15154648d1a3a6c7b1c
Kathy Walrath <kathyw@google.com>
2013-02-07 13:55:03 -0800

    add HTML example from book (much updated)

---
+++ code/ch03/ch03_2_html/web/ch03_2_html.dart
@@ -0,0 +1,60 @@
+import 'dart:html';
+import 'dart:async';
+import 'dart:json' as json;
+
+// A JSON-formatted file in the same location as this page.
+var jsonUri = 'data.json';
+// Besides 'data.json', also try 'nodata.json' and 'nofile.json'.
+
+// An XML-formatted file in the same location as this page.
+var xmlUri = 'data.xml';
+// Besides 'data.xml', also try 'nodata.xml' and 'nofile.xml'.
+
+main() {
+  // Read a JSON file.
+  HttpRequest.getString(jsonUri)
+      .then(processString)
+      .catchError(handleError);
+  
+  // Read an XML file.
+  HttpRequest.request(xmlUri)
+      .then(processRequest)
+      .catchError(handleError);
+}
+
+processString(String jsonText) {
+  parseText(jsonText);
+}
+
+processRequest(HttpRequest request) {
+  var xmlDoc = request.responseXml;
+  try {
+    var license = xmlDoc.query('license').text;
+    print('License: $license');
+  } catch(e) {
+    print('$xmlUri doesn\'t have correct XML formatting.');
+  }
+}
+
+parseText(String jsonText) {
+  var response;
+  
+  try {
+    response = json.parse(jsonText);
+  } on FormatException catch(e) {
+    print('$jsonUri doesn\'t have correct JSON formatting.');
+    print(e.message);
+    return;
+  }
+
+  String name = response['name'];
+  String license = response['license'];
+  print('The $name project uses the $license license.');
+}
+
+// PENDING: We should do more explicit error handling, once it's available. 
+handleError(AsyncError error) {
+  print('Uh oh, there was an error.');
+  print(error.toString());
+}
+
