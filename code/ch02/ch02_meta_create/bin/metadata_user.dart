ec850238cd4b96656e6d5772252685dcdf5bec58
Kathy Walrath <kathyw@google.com>
2013-02-25 14:41:09 -0800

    New metadata example: implementing an annotation; moved old metadata example
    Review URL: https://chromiumcodereview.appspot.com/12310119

---
+++ code/ch02/ch02_meta_create/bin/metadata_user.dart
@@ -0,0 +1,14 @@
+import 'todo.dart';
+//import 'dart:mirrors';
+
+/// Do something.
+@todo('Seth', 'Make sure this code always works')
+void doSomething() {
+  print('do something');
+}
+
+main() {
+  // PENDING: Do some reflection on doSomething
+  // to see the annotation.
+  print('tbd');
+}
---
+++ code/ch02/ch02_meta_create/bin/todo.dart
@@ -0,0 +1,11 @@
+library todo;
+
+const todo = _ToDo;
+
+class _ToDo {
+  String who = 'unassigned';
+  String what = 'unspecified';
+  
+  _ToDo(this.who, this.what);
+}
+
