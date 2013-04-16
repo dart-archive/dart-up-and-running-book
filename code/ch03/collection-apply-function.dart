ee616e0bf1c34ab34ebbbd270d0d3a55430e0c2f
Kathy Walrath <kathyw@google.com>
2013-04-16 11:28:24 -0700

    remove Collection from the book
    Staged at URLs like:
    
    http://book.dart-lang.appspot.com/docs/dart-up-and-running/contents/ch03.html#ch03-collections
    
    Review URL: https://chromiumcodereview.appspot.com/13991008

--- code/ch03/collection-apply-function.dart
+++ code/ch03/collection-apply-function.dart
@@ -3,8 +3,10 @@ main() {
   
   teas.forEach((tea) => print('I drink $tea'));
   
-  var loudTeas = teas.map((tea) => tea.toUpperCase()).toList();
+  var loudTeas = teas.map((tea) => tea.toUpperCase());
   assert(loudTeas.any((tea) => tea == 'GREEN'));
-  
   loudTeas.forEach(print);
+  
+  var loudTeaList = teas.map((tea) => tea.toUpperCase()).toList();
+  loudTeaList.forEach(print);
 }
