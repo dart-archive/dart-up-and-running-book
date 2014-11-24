a209263e969c59ca4309259c1e357c60c037cd1c
Kathy Walrath <kathyw@google.com>
2014-11-24 15:11:40 -0800

    Add enum code

---
+++ code/ch02/enum_switch.dart
@@ -0,0 +1,16 @@
+enum Color { red, green, blue }
+
+main() {
+  var aColor = Color.red;
+  
+  switch (aColor) {
+    case Color.red:
+      print('Red as roses!');
+      break;
+    case Color.green:
+      print('Green as grass!');
+      break;
+    default:
+      print(aColor);
+  }
+}
