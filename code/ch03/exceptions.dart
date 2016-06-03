9364da6233bc647481c22af84d37fa98f4b33db1
Kathy Walrath <kathyw@google.com>
2016-06-03 14:19:03 -0700

    Update exceptions.dart

--- code/ch03/exceptions.dart
+++ code/ch03/exceptions.dart
@@ -5,5 +5,5 @@ main() {
 class FooException implements Exception {
   final String msg;
   const FooException([this.msg]);
-  String toString() => msg == null ? 'FooException' : msg;
+  String toString() => msg ?? 'FooException (no message)';
 }
