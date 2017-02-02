bb959fce373e0ad5a6102dd834cfcf10691333d8
Kathy Walrath <kathyw@google.com>
2017-02-02 14:40:59 -0800

    Update optional parameter syntax

--- code/language-tour/list-map-default-function-parameters/bin/main.dart
+++ code/language-tour/list-map-default-function-parameters/bin/main.dart
@@ -1,6 +1,6 @@
 void doStuff(
-    {List<int> list: const [1, 2, 3],
-    Map<String, String> gifts: const {
+    {List<int> list = const [1, 2, 3],
+    Map<String, String> gifts = const {
       'first': 'paper',
       'second': 'cotton',
       'third': 'leather'
